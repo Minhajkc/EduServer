e72c0cd826ee4c5566f3fe5a1e3cb2156e18d2f7
minhajkc <minhajkc00@gmail.com>
2024-11-08 11:55:49 +0530

    Cookie prblem

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(cors({
     origin: 'https://eduprops.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(cookieParser());
